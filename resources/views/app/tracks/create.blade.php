f14b3ba4ef2bd35236889ae02842cf66f477fe5a
Théo Pantene <theo.pantene@etu.unilim.fr>
2024-10-25 13:45:56 +0200

    Ajout du champs de selection de categorie dans create blade

--- resources/views/app/tracks/create.blade.php
+++ resources/views/app/tracks/create.blade.php
@@ -35,6 +35,20 @@
                 <p class="error-message">{{ $message }}</p>
                 @enderror
 
+                <div>
+                    <label for="category_id">Catégorie</label>
+                    <select name="category_id" id="category_id">
+                        <option value="" selected disabled>Sélectionner une catégorie</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                @error('category_id')
+                <p class="error-message">{{ $message }}</p>
+                @enderror
+
                 <div class="submit">
                     @csrf
                     <button type="submit" class="primary">Envoyer</button>
